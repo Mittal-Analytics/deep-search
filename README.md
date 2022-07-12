7371420fd2b9ebddb56c57a9a6de7dbdc7db7231
Lakshay Mittal <lakshaymittal@192.168.10.204>
2022-07-12 18:32:05 +0530

    Reduced to single file

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # deep-search
 Library for creating custom Google Search.
+
+Current functionality:
+-> Sorts the common urls among different search queries and all their results, required apperance of a url to be excluded is atleast 50%.
--- deep-search.py
+++ deep-search.py
@@ -1,5 +1,6 @@
+import csv
+from urllib.parse import urlparse
 from googleapiclient.discovery import build
-import json
 
 companies = ["Avanti Feeds", 
              "Acrysil", 
@@ -13,25 +14,94 @@ service = build(
     "customsearch", "v1", developerKey="AIzaSyDjL9Kcfl6O2Zvl_2alvqXSPsAnba0hEhw"
 )
 
-for a in companies:
-    tot_res = []
+for company in companies:
 
     for i in range(1, 100, 10):
         res = (
             service.cse()
             .list(
-                q=a,
+                q=company,
                 cx="cea393e795c307f0f",
                 start = i
             )
             .execute()
         )
 
-        for j in res["items"]:
-            tot_res.append(j["link"])
+        if(res):
+            for j in res["items"]:
+                results.append(j["link"])
 
-    results.append(tot_res)
+data = []
 
-with open("data.json", "w") as jsonFile:
-    json.dump(results, jsonFile)
-    
+for i in results:
+    url = urlparse(i)
+    data.append(
+        {
+            "domain": url.netloc,
+            "path": url.path
+        }
+    )
+
+############
+
+## The comparison happens now
+
+remainder = []
+
+## Longest common path
+
+def longestCommonPath(a, b):
+    i = 0
+    while a[i] == b[i]:
+        if i+1 == len(a) or i+1 == len(b):
+            break
+        i+=1
+    while a[i] != '/':
+        i-=1
+    return a[0:i+1]
+
+## Filter URL
+
+def filterURL(url):
+    flag1 = 1
+    flag2 = 1
+
+    for i in remainder:
+        if i["domain"] == url["domain"]:
+            flag1 = 0
+            if i["path"] in url["path"]:
+                flag2 = 0
+                i["seen"] += 1
+                break
+            else: ci = i
+    if flag1:
+        remainder.append({
+            "domain": url["domain"],
+            "path": url["path"],
+            "seen": 1
+        })
+    elif flag2:
+        remainder.append({
+            "domain": url["domain"],
+            "path": longestCommonPath(ci["path"], url["path"]),
+            "seen": ci["seen"] + 1
+        })
+
+## Filtering Happens
+
+for i in data:
+    filterURL(i)
+
+## Writing to the file
+
+blacklist = []
+
+for i in remainder:
+    if i["seen"] > len(data)/200:
+        blacklist.append(i["domain"]+i["path"])
+        
+with open("final.tsv", "w") as writeFile:
+    tsv_writer = csv.writer(writeFile, delimiter='\t')
+    tsv_writer.writerow(["URL", "Label"])
+    for i in blacklist:
+        tsv_writer.writerow([i+'*', "_exclude_"])
