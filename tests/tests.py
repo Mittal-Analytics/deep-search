49d8945fdd8ca6cb6f0832564b17d49dd4c20baa
Lakshay Mittal <lakshaymittal225@gmail.com>
2022-07-28 19:39:41 +0530

    New test cases added for helper function

--- tests/tests.py
+++ tests/tests.py
@@ -2,7 +2,11 @@ import logging
 import os
 import unittest
 
-from src.deep_search import find_blacklist_urls
+from src.deep_search import (
+    _longest_common_path,
+    _update_links_list,
+    find_blacklist_urls,
+)
 
 logging.basicConfig(level=logging.INFO)
 
@@ -27,6 +31,26 @@ class DeepSearchTests(unittest.TestCase):
             queries, CX, GOOGLE_CLOUD_KEY, CACHE_VERSION
         )
         self.assertTrue("www.screener.in/company/" in blacklist)
+
+    def test_longest_common_path(self):
+        path = _longest_common_path("/abc/xyz/ijk/", "/abc/xyz/ilu")
+        self.assertEqual(path, "/abc/xyz/")
+
+    def test_update_links_list(self):
+        links_list = [
+            {"domain": "www.screener.com", "path": "/company/", "seen": 1},
+            {"domain": "www.trendlyne.com", "path": "/equity/", "seen": 1},
+        ]
+        links = [
+            {"domain": "www.screener.com", "path": "/company/itc/"},
+            {"domain": "www.trendlyne.com", "path": "/"},
+        ]
+        _update_links_list(links, links_list)
+        expected = [
+            {"domain": "www.screener.com", "path": "/company/", "seen": 2},
+            {"domain": "www.trendlyne.com", "path": "/", "seen": 2},
+        ]
+        self.assertEqual(expected, links_list)
 
 
 if __name__ == "__main__":
